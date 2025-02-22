938dac80fcac7632d10cff2685a475dda787f2e0
Jenny Heath <jheath@codeforamerica.org>
2025-02-21 19:27:06 -0800

    update bio

--- src/components/About.js
+++ src/components/About.js
@@ -8,37 +8,49 @@ import {ModalTitle, ModalContent} from './styled/ModalComponents'
 import Sel from '../static/sel.png';
 
 const AboutModalContent = styled(ModalContent)`
-  display: flex;
-  a {
-    color: #dd9593;
-    text-decoration: none;
-  }
+    display: flex;
+
+    a {
+        color: #dd9593;
+        text-decoration: none;
+    }
 `;
 
 const BioImg = styled.img`
-  height: 200px;
-  margin: 10px 0px 10px 10px;
-  border-radius: 50%;
+    height: 200px;
+    margin: 10px 0px 10px 10px;
+    border-radius: 50%;
 `;
 
 const Name = styled.span`
-  font-size: 30px;
+    font-size: 30px;
 `;
 
 const About = () => {
-    return (
-      <div>
-        <Home />
-        <CustomModal>
-          <AboutModalContent>
-            <div>
-              <Name>Selene Ross</Name> is an audio producer, musician and artist. She has worked on stories for Radiotopia's The Kitchen Sisters, NPR Member Station KALW, independent podcasts, and has helped people turn their original concepts into artistically produced audio stories. As a voice actor, she has voiced projects ranging from <a href='https://www.replicant.ai' target='_blank'>San Francisco start-up's</a> to <a href='https://polology.com/yes-the-church-of-google-exists/' target='_blank'>independent films</a>. Selene is based out of her hometown of Berkeley, CA, but lived previously in Berlin, Germany, where she organized and performed at poetry and <a href='https://amandasparish.com/2016/06/22/and-surrender/' target='_blank'>performance art exhibitions</a> and edited <a href='https://issuu.com/berlinspokenword/docs/unspokenvolume1' target='_blank'>Berlin's spoken word magazine</a>. She holds two B.A.’s in Environmental Studies and Sociology from the University of California, Santa Barbara.
-            </div>
-            <BioImg src={Sel} />
-          </AboutModalContent>
-        </CustomModal>
-      </div>
-    );
+  return (
+  <div>
+    <Home/>
+    <CustomModal>
+      <AboutModalContent>
+        <div>
+          <Name>Selene Ross (she/her)</Name> is an audio producer and writer from Berkeley, California,
+          and a big believer in the power of story and sound. Her audio stories have aired on Radiotopia's
+          The Kitchen Sisters, KALW, and NPR, and her writing appears in Literary Hub, Terrain.org, and
+          other publications. Her work has been supported by residencies with the Spring Creek Project,
+          Voices of the Wilderness, and Storyknife Writers Retreat. She was senior audio producer at
+          Dipsea, where she directed voice actors, managed sound-design, and led the development of a new
+          genre of sleep audio. Her Oakland-based musical trio Artemisia features harp, violin, and tight
+          vocal harmonies with a strong Appalachian influence.
+          Selene teaches podcast storytelling and writing at Oregon State University and Portland
+          Community College. She has a BA in Environmental Studies and Sociology from the University of
+          California, Santa Barbara, an MFA in Creative Writing: Fiction from Oregon State University, and
+          a love of all things weird and wonderful.
+        </div>
+        <BioImg src={Sel}/>
+      </AboutModalContent>
+    </CustomModal>
+  </div>
+  );
 }
 
 export default hot(module)(About);
